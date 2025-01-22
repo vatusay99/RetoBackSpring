8b76de648066309e1bb3ba741b31b55ee020e252
Sinedin <sinedindnd@hotmail.com>
2025-01-22 16:04:44 -0500

    creacion del service y eliminacion de archivos grade que no se requieren

--- src/main/java/com/crud_products_retotecnico/repository/ProductRepository.java
+++ src/main/java/com/crud_products_retotecnico/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.crud_products_retotecnico.repository;
 
-public interface ProductRepository {
+import com.crud_products_retotecnico.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 }
---
+++ src/main/java/com/crud_products_retotecnico/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.crud_products_retotecnico.service;
+
+import com.crud_products_retotecnico.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    Product save(Product product);
+
+    List<Product> findAll();
+    Product findById(Integer id);
+
+    void deleteById(Integer id);
+
+    Product update(Product product);
+}
